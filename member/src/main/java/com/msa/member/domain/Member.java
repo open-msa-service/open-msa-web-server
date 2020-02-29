ff15668e0ee1399ef115e8821571db9bcd106f78
jongminelee0 <jongminlee0@gmail.com>
2020-02-29 16:28:10 +0900

    add : message

--- member/src/main/java/com/msa/member/domain/Member.java
+++ member/src/main/java/com/msa/member/domain/Member.java
@@ -44,6 +44,12 @@ public class Member {
     @Column(name = "PHONE_NUMBER", nullable = false)
     private String phoneNumber;
 
+    @Column(name = "STATUS_MESSAGE")
+    private String statusMessage;
+
+    @Column(name = "INTRODUCE_MESSAGE")
+    private String introduceMessge;
+
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Column(name = "UPDATE_TIME", nullable = false)
     private LocalDateTime updateTime;
