a040ddfd02d30b0896b22f068598b4839f7076d6
jongminelee0 <jongminlee0@gmail.com>
2020-03-21 14:00:18 +0900

    add : circuit breaker config

--- msa-gateway/src/main/java/msa/demo/gateway/GatewayApplication.java
+++ msa-gateway/src/main/java/msa/demo/gateway/GatewayApplication.java
@@ -1,9 +1,14 @@
 package msa.demo.gateway;
 
+import msa.demo.gateway.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
+@EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
---
+++ msa-gateway/src/main/java/msa/demo/gateway/config/RibbonConfiguration.java
@@ -0,0 +1,28 @@
+package msa.demo.gateway.config;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import org.springframework.context.annotation.Bean;
+
+/**
+ *  default 로드 밸런싱 설정 변경
+ */
+public class RibbonConfiguration {
+
+    @Bean
+    public IPing ribbonPing(final IClientConfig config){
+        // service 상태 확인
+        return new PingUrl(false, "/health");
+    }
+
+    @Bean
+    public IRule ribbonRule(final IClientConfig config){
+        // 기본적인 round-robin을 대체한다
+        // 핑을 보내 응답하지 않는 인스턴스를 건너뛴다.
+        return new AvailabilityFilteringRule();
+    }
+
+}
